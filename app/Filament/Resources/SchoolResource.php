e7fbbbfe564ba802fba9c37b2b76e7b787f3c605
erdi20 <rayadavid158@gmail.com>
2025-05-27 21:45:51 +0700

    feat: fitur pengaturan web sekolah (belum fix)

---
+++ app/Filament/Resources/SchoolResource.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SchoolResource\Pages;
+use App\Filament\Resources\SchoolResource\RelationManagers;
+use App\Models\School;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Table;
+use Filament\Forms;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SchoolResource extends Resource
+{
+    protected static ?string $model = School::class;
+    protected static ?string $pluralLabel = 'Sekolah';
+    protected static ?string $label = 'Pengaturan web';
+
+    protected static ?string $slug = 'Sekolah';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('tagline')
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('address')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required(),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required(),
+                Forms\Components\TextInput::make('whatsapp')
+                    ->required(),
+                Forms\Components\TextInput::make('instagram')
+                    ->required(),
+                Forms\Components\TextInput::make('facebook')
+                    ->required(),
+                Forms\Components\TextInput::make('youtube')
+                    ->required(),
+                Forms\Components\TextInput::make('logo')
+                    ->required(),
+                Forms\Components\TextInput::make('favicon')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tagline')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('whatsapp')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('instagram')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('facebook')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('youtube')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('logo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('favicon')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchools::route('/'),
+            'create' => Pages\CreateSchool::route('/create'),
+            'edit' => Pages\EditSchool::route('/{record}/edit'),
+        ];
+    }
+}
